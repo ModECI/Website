82d4baf14153902c09df865708ef0d82446a98b5
vidhya-metacell <71261494+vidhya-metacell@users.noreply.github.com>
2021-05-12 13:19:10 +0530

    #14 Make overall scrolling to pages from privacy page work

--- js/modeci.js
+++ js/modeci.js
@@ -6,16 +6,6 @@
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top-100
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
-});
-
-$(function() {
     $('a.anchor-scroll').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
